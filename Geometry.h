e73be0fbadb36cb49d68e4fe5796cca8b54a25c4
logan <loganhoelscher123@gmail.com>
2021-05-04 12:56:57 -0500

    Add project files.

---
+++ Geometry.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+
+// GLEW
+#define GLEW_STATIC
+#include <glew.h>
+
+
+// GLFW
+#include <glfw3.h>
+
+
+// GLM Mathematics
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+
+class Shape
+{
+	
+public: 
+	
+    
+};
+
+
+
